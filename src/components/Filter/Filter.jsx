25688eeded1890855ab2392e1fdf67fe56649f4e
usmanmani0 <usmanmani010@gmail.com>
2022-01-03 19:05:07 +0500

    homepage fixed missing items

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,18 +1,15 @@
 import React, { useState, useEffect } from "react";
-import Chip from '@mui/material/Chip';
-import Autocomplete from '@mui/material/Autocomplete';
-import TextField from '@mui/material/TextField';
-import Stack from '@mui/material/Stack';
+import Chip from "@mui/material/Chip";
+import Autocomplete from "@mui/material/Autocomplete";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
 import "./filter.css";
 import { BsChevronDown } from "react-icons/bs";
 import { BsFilter } from "react-icons/bs";
 import { BiChevronRight } from "react-icons/bi";
-import RightArrow from "../../assets/images/right-arrow.png"
-
-import img1 from "../../assets/images/s.png"
-
-
-
+import RightArrow from "../../assets/images/right-arrow.png";
+import { DropdownButton, Dropdown } from "react-bootstrap";
+import img1 from "../../assets/images/s.png";
 
 function Filter() {
   function myFunction() {
@@ -81,15 +78,14 @@ function Filter() {
     // setList(data);
   }, [list]);
 
-  const handleSelectAll = e => {
+  const handleSelectAll = (e) => {
     // alert("ALL")
     setIsCheckAll(!isCheckAll);
-    SetSelectedFilter(list.map(li => `${li.typeis}`));
+    SetSelectedFilter(list.map((li) => `${li.typeis}`));
     if (isCheckAll) {
       SetSelectedFilter([]);
     }
   };
-
 
   const handleClick = (e, name) => {
     // alert("JJ")
@@ -97,7 +93,7 @@ function Filter() {
     let present = selectedFilter.find((data) => data == name);
     console.log("PRESENT", present);
     if (present) {
-      SetSelectedFilter(selectedFilter.filter(item => item !== name));
+      SetSelectedFilter(selectedFilter.filter((item) => item !== name));
     } else {
       SetSelectedFilter([...selectedFilter, name]);
     }
@@ -105,10 +101,7 @@ function Filter() {
   const enteringAutoComplete = (value) => {
     console.log("AUTOVALUE", value);
     SetSelectedFilter((preValue) => {
-      return (
-        value
-      );
-
+      return value;
     });
   };
 
@@ -123,18 +116,22 @@ function Filter() {
               type="button"
             >
               <i className="icon_filter" style={{ marginLeft: "5px" }}>
-                {selectedFilter.length == 0 ? <BsFilter className="icon" size="16px" color="black" /> : <div className="filter_count_app_card">{selectedFilter.length}</div>}
-
+                {selectedFilter.length == 0 ? (
+                  <BsFilter className="icon" size="16px" color="black" />
+                ) : (
+                  <div className="filter_count_app_card">
+                    {selectedFilter.length}
+                  </div>
+                )}
               </i>
               <span>Filter</span>
             </button>
           </div>
           {show ? (
             <div className="filter_expand">
-
-              <div className="filter_input">
-                <img src={img1} className="search" />
-                <Stack spacing={1} style={{ width: "95%" }} >
+              <div className="filter_input_element">
+                <img src={img1} className="search_element" />
+                <Stack spacing={1} style={{ width: "95%" }}>
                   <Autocomplete
                     multiple
                     id="tags-filled"
@@ -143,71 +140,107 @@ function Filter() {
                     // defaultValue={[list[3].title]}
                     // freeSolo
                     onChange={(e, v) => enteringAutoComplete(v)}
-                    renderTags={(value, getTagProps) => value.map((option, index) => (
-                      <Chip variant="outlined" label={option} {...getTagProps({ index })} />
-                    ))}
+                    renderTags={(value, getTagProps) =>
+                      value.map((option, index) => (
+                        <Chip
+                          variant="outlined"
+                          label={option}
+                          {...getTagProps({ index })}
+                        />
+                      ))
+                    }
                     renderInput={(params) => (
                       <TextField
                         {...params}
                         variant="filled"
                         label=""
-                        placeholder="Search Elements" />
-                    )} />
+                        placeholder="Search Elements"
+                      />
+                    )}
+                  />
                 </Stack>
               </div>
 
-
               <hr className="bottom_line"></hr>
 
-              <div className='UiSecreen_app_categories_button'>
-
-                <div className="d-flex"
-                  style={{ color: "black" }}>Elements</div>
+              <div className="UiSecreen_app_categories_button">
+                <div className="d-flex" style={{ color: "black" }}>
+                  Elements
+                </div>
                 <div>
                   <img src={RightArrow} />
                 </div>
-
-
               </div>
-
 
               <div className="checkbox_div">
                 <input
-
                   type="checkbox"
                   onClick={handleSelectAll}
-                  isChecked={isCheckAll} />
+                  isChecked={isCheckAll}
+                />
                 <label className="label">All</label>{" "}
               </div>
               <hr className="vertical_line"></hr>
               {list.map((data, index) => {
                 return (
                   <>
-                    <div className="checkbox_div">
-
+                    <div className="checkbox_div" key={index}>
                       <input
-
                         key={data.id}
                         type="checkbox"
                         id={data.id}
                         onClick={(e) => handleClick(e, data.typeis)}
-                        checked={selectedFilter.includes(`${data.typeis}`)} />
+                        checked={selectedFilter.includes(`${data.typeis}`)}
+                      />
                       <label className="label">{data.typeis}</label>{" "}
                     </div>
                     <hr className="vertical_line"></hr>
-
                   </>
                 );
               })}
             </div>
           ) : null}
           <div>
-            <button className="recent_button" type="button">
+            {/* <button className="recent_button" type="button">
               <span>Recently Updated (All)</span>
               <i className="icon_down">
                 <BsChevronDown className="icon" size="16px" color="black" />
               </i>
-            </button>
+            </button> */}
+
+            {/* <DropdownButton id="dropdown-basic-button" title="Dropdown button">
+              <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+            </DropdownButton> */}
+
+            <div class="dropdown">
+              <button
+                class="dropdown-toggle"
+                type="button"
+                id="dropdownMenuButton1"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+                className="recent_button"
+              >
+                <span>Recently Updated (All)</span>
+                <i className="icon_down">
+                  <BsChevronDown className="icon" size="16px" color="black" />
+                </i>
+              </button>
+              <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                <li>
+                  <a class="dropdown-item" href="#">
+                    <span>Recently Updated (All)</span>
+                  </a>
+                </li>
+                <li>
+                  <a class="dropdown-item" href="#">
+                    <span>Alphabetically (A-Z)</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,24 +1,23 @@
-import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
-import "./footer.css"
-
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import "./footer.css";
 
 const Footer = () => {
-    return (
-        <>
-        <div className='footer_div'>
-            <Container>
-            <div className='footer_desc'>
-            <div className='footerheading_row'>
-            <h4 className='footer_heading'>Cookie Policy</h4>
-            <h4 className='footer_heading'>Privacy Policy</h4>
-            <h4 className='footer_heading'>Provide Feedback</h4>
+  return (
+    <>
+      <div className="footer_div">
+        <Container>
+          <div className="footer_desc">
+            <div className="footerheading_row">
+              <h4 className="footer_heading">Cookie Policy</h4>
+              <h4 className="footer_heading">Privacy Policy</h4>
+              <h4 className="footer_heading">Provide Feedback</h4>
             </div>
-            </div>
-            </Container>
-        </div>
-        </>
-    )
-}
+          </div>
+        </Container>
+      </div>
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
