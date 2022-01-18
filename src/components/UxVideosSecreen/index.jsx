79d3027e11c8cfa2da01f8506fb8358e2414a403
Afzal00786 <afzaltrigonsoft@gmail.com>
2022-01-18 01:12:26 -0800

    sf

--- src/components/UxVideosSecreen/index.jsx
+++ src/components/UxVideosSecreen/index.jsx
@@ -152,199 +152,201 @@ const UxVideoSecreen = () => {
             return (
               <>
                 <div key={index}>
-                  <div className="vedio_card">
-                    <Link to="/soundcloudpage">
+                  <Link to="/soundcloudpage">
+                    <div className="vedio_card">
+
                       {" "}
                       <img src={data.img} className="img-fluid" alt="err" />
-                    </Link>
-                    <div className="UI_Secreen_add_collection">
-                      {showPopover ? (
-                        <div
-                          className="UI_Secreen_popoover_wrapper"
-                          style={{
-                            display:
-                              index === showPopover.open ? "block" : "none",
-                          }}
-                        >
-                          <div class="form-group Ui_Secreen_has-search ">
-                            <input
-                              type="text"
-                              class="form-control"
-                              id="form_control"
-                              placeholder="Search Collections"
-                            ></input>
-                          </div>
 
-                          {
+                      <div className="UI_Secreen_add_collection">
+                        {showPopover ? (
+                          <div
+                            className="UI_Secreen_popoover_wrapper"
+                            style={{
+                              display:
+                                index === showPopover.open ? "block" : "none",
+                            }}
+                          >
+                            <div class="form-group Ui_Secreen_has-search ">
+                              <input
+                                type="text"
+                                class="form-control"
+                                id="form_control"
+                                placeholder="Search Collections"
+                              ></input>
+                            </div>
 
-                            checkdata.map((data, index) => {
-                              return (
-                                <div
-                                  className="UI_Secreen_collection_checkbox_wrapper"
-                                  key={index}
-                                >
-                                  <div className="Ui_Secreen_collection_inner_wrapper">
-                                    <div
-                                      className={
-                                        tick.includes(`${index}`)
-                                          ? "Ui_Secreen_checkbox_wrapper_enable"
-                                          : "Ui_Secreen_checkbox_wrapper"
-                                      }
-                                      onClick={() => checkTick(`${index}`)}
-                                    >
-                                      {tick.includes(`${index}`) ? (
-                                        <Image src={data.image} />
-                                      ) : (
-                                        ""
-                                      )}
-                                    </div>
-                                    <div>
-                                      <span className="collection_txt">
-                                        <EditText
-                                          type={"text"}
-                                          name={"Name"}
-                                          value={data.Name}
-                                          onChange={(e) => changeDynamicText(e, index)}
-                                        />
+                            {
 
-
+                              checkdata.map((data, index) => {
+                                return (
+                                  <div
+                                    className="UI_Secreen_collection_checkbox_wrapper"
+                                    key={index}
+                                  >
+                                    <div className="Ui_Secreen_collection_inner_wrapper">
+                                      <div
+                                        className={
+                                          tick.includes(`${index}`)
+                                            ? "Ui_Secreen_checkbox_wrapper_enable"
+                                            : "Ui_Secreen_checkbox_wrapper"
+                                        }
+                                        onClick={() => checkTick(`${index}`)}
+                                      >
+                                        {tick.includes(`${index}`) ? (
+                                          <Image src={data.image} />
+                                        ) : (
+                                          ""
+                                        )}
+                                      </div>
+                                      <div>
+                                        <span className="collection_txt">
+                                          <EditText
+                                            type={"text"}
+                                            name={"Name"}
+                                            value={data.Name}
+                                            onChange={(e) => changeDynamicText(e, index)}
+                                          />
 
 
-                                      </span>
-                                    </div>
-                                    {/* <div>
+
+
+                                        </span>
+                                      </div>
+                                      {/* <div>
                                     <img
                                       src={PlayVedio}
                                       className="play_vedio_add_collection"
                                       alt="err"
                                     />
                                   </div> */}
+                                    </div>
                                   </div>
-                                </div>
-                              );
-                            })}
+                                );
+                              })}
 
-                          <div className="UI_Secreen_save_collection_btn_wrapper">
-                            <div
-                              className="add_collection_btn"
-                              onClick={() => addCollection()}
-                            >
-                              <Image src={plusiconsave} />
-                              <span className="nc_txt">New Collection</span>
-                            </div>
-                            <div
-                              className={
-                                tick.length >= 1
-                                  ? "save_coll_enable_btn"
-                                  : " save_collection_btn"
-                              }
-                              onClick={
-                                tick.length >= 1 ? () => saveCollection() : null
-                              }
-                            >
-                              {" "}
-                              <span
-                                className="save_txt"
-                                onClick={handelPopover}
+                            <div className="UI_Secreen_save_collection_btn_wrapper">
+                              <div
+                                className="add_collection_btn"
+                                onClick={() => addCollection()}
                               >
-                                Save
-                              </span>{" "}
+                                <Image src={plusiconsave} />
+                                <span className="nc_txt">New Collection</span>
+                              </div>
+                              <div
+                                className={
+                                  tick.length >= 1
+                                    ? "save_coll_enable_btn"
+                                    : " save_collection_btn"
+                                }
+                                onClick={
+                                  tick.length >= 1 ? () => saveCollection() : null
+                                }
+                              >
+                                {" "}
+                                <span
+                                  className="save_txt"
+                                  onClick={handelPopover}
+                                >
+                                  Save
+                                </span>{" "}
+                              </div>
                             </div>
                           </div>
+                        ) : null}
+                      </div>
+                      <div
+                        className="share_copy"
+                        onClick={() => {
+                          setSelect({
+                            open: select.open === index ? false : index,
+                          });
+                        }}
+                        style={{
+                          display: index === select.open ? "block" : "none",
+                        }}
+                      >
+                        <div className="select_content_wrapper_1">
+                          <div
+                            className="d-flex align-items-center"
+                            onClick={() => {
+                              setShowPopover({
+                                open: showPopover.open === index ? false : index,
+                              });
+                            }}
+                          >
+                            <div>
+                              <img src={Plus} alt="err" />
+                            </div>
+                            <div className="add_to_col">Add to Collection</div>
+                          </div>
+                          <div className="d-flex align-items-center mt-2">
+                            <div>
+                              <img src={Down} style={{ marginLeft: "-7px" }} alt="err" />
+                            </div>
+                            <div className="select_download">Download</div>
+                          </div>
+                          <div className="d-flex align-items-center mt-2">
+                            <div>
+                              <img src={Copy} alt="err" />
+                            </div>
+                            <div className="add_to_col" onClick={() => {
+                              copyShare()
+                            }}>Copy Share Link</div>
+                          </div>
                         </div>
-                      ) : null}
-                    </div>
-                    <div
-                      className="share_copy"
-                      onClick={() => {
-                        setSelect({
-                          open: select.open === index ? false : index,
-                        });
-                      }}
-                      style={{
-                        display: index === select.open ? "block" : "none",
-                      }}
-                    >
-                      <div className="select_content_wrapper_1">
-                        <div
-                          className="d-flex align-items-center"
-                          onClick={() => {
+                      </div>
+                      <div className="mobie_share_copy"
+                        onClick={() => {
+                          setSelect({
+                            open: select.open === index ? false : index,
+                          });
+                        }}
+                        style={{
+                          display: index === select.open ? "block" : "none",
+                        }}>
+                        <div className="d-flex justify-content-center">
+                          <img src={MPlus} onClick={() => {
                             setShowPopover({
                               open: showPopover.open === index ? false : index,
                             });
-                          }}
-                        >
-                          <div>
-                            <img src={Plus} alt="err" />
-                          </div>
-                          <div className="add_to_col">Add to Collection</div>
+                          }} />
                         </div>
-                        <div className="d-flex align-items-center mt-2">
-                          <div>
-                            <img src={Down} style={{ marginLeft: "-7px" }} alt="err" />
-                          </div>
-                          <div className="select_download">Download</div>
+                        <div className="d-flex justify-content-center">
+                          <img src={MDownload} />
                         </div>
-                        <div className="d-flex align-items-center mt-2">
-                          <div>
-                            <img src={Copy} alt="err" />
-                          </div>
-                          <div className="add_to_col" onClick={() => {
-                            copyShare()
-                          }}>Copy Share Link</div>
+                        <div className="d-flex justify-content-center" onClick={copyShare}>
+                          <img src={MCopy} />
                         </div>
                       </div>
-                    </div>
-                    <div className="mobie_share_copy"
-                      onClick={() => {
-                        setSelect({
-                          open: select.open === index ? false : index,
-                        });
-                      }}
-                      style={{
-                        display: index === select.open ? "block" : "none",
-                      }}>
-                      <div className="d-flex justify-content-center">
-                        <img src={MPlus} onClick={() => {
-                          setShowPopover({
-                            open: showPopover.open === index ? false : index,
-                          });
-                        }} />
-                      </div>
-                      <div className="d-flex justify-content-center">
-                        <img src={MDownload} />
-                      </div>
-                      <div className="d-flex justify-content-center" onClick={copyShare}>
-                        <img src={MCopy} />
-                      </div>
-                    </div>
 
-                    <div className="vedio_time">
-                      <div className="vedio_time_wrapper">12:40</div>
-                    </div>
+                      <div className="vedio_time">
+                        <div className="vedio_time_wrapper">12:40</div>
+                      </div>
 
-                    <div className="card_content">
-                      <div className="card_content_show">
-                        <div className="d-flex">
-                          <div>
-                            {" "}
-                            <img src={Cloud} className="onHoverImage" alt="err" />
+                      <div className="card_content">
+                        <div className="card_content_show">
+                          <div className="d-flex">
+                            <div>
+                              {" "}
+                              <img src={Cloud} className="onHoverImage" alt="err" />
+                            </div>
+                            <div className="onHoverText">{data.text}</div>
                           </div>
-                          <div className="onHoverText">{data.text}</div>
-                        </div>
-                        <div
-                          className="select_btn"
-                          onClick={() => {
-                            setSelect({
-                              open: select.open === index ? false : index,
-                            });
-                          }}
-                        >
-                          <img src={option} alt="err" />{" "}
+                          <div
+                            className="select_btn"
+                            onClick={() => {
+                              setSelect({
+                                open: select.open === index ? false : index,
+                              });
+                            }}
+                          >
+                            <img src={option} alt="err" />{" "}
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
+                  </Link>
                   <div className="card_botom_text">{data.text}</div>
                 </div>
                 {setSave ? (
